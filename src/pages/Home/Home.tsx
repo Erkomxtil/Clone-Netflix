19729bd7e0b22b5aa5d280a9921981c7dfafceb7
Delastef <delastef@hotmail.comgit config --global user.name delastefegit config --global user.email delastef@hotmail.com>
2023-10-10 16:16:10 +0200

    Ajout de la traduction pour la faq

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,33 +7,33 @@ import { ProfilDownload } from "../../components/ProfilDownload"
 import { Accordion } from "../../components/Accordion"
 import { Footer } from "../../components/Footer"
 
-
-import { AccordionDatas } from "../../datas/AccordionDatas"
-
 import { useContext, useEffect, useState } from "react"
 import { UserContext } from "../../utils/context/languageContext"
 
 import { WatchDatasFr, WatchDatasEn } from "../../datas/WatchDatas"
 import { ProfilDatasFr, ProfilDatasEn } from "../../datas/ProfilDatas"
+import { AccordionDatasFr, AccordionDatasEn } from "../../datas/AccordionDatas"
 
 import { WatchProps } from "../../shared/interfaces/watch.interface"
 import { ProfilProps } from "../../shared/interfaces/profil.interface"
-
-
+import { FaqProps } from "../../shared/interfaces/faq.interface"
 
 export const Home = () => {
   const {language} = useContext(UserContext)
   const [watch, setWatch] = useState<WatchProps[]>()
   const [profil, setProfil] = useState<ProfilProps[]>()
+  const [faq, setFaq] = useState<FaqProps[]>()
 
 useEffect(()=> {
   if(language === "fr") {
     setWatch(WatchDatasFr)
     setProfil(ProfilDatasFr)
+    setFaq(AccordionDatasFr)
   }
   if(language === "en") {
     setWatch(WatchDatasEn)
     setProfil(ProfilDatasEn)
+    setFaq(AccordionDatasEn)
   }
 },[language])
 
@@ -74,15 +74,17 @@ useEffect(()=> {
       <div className="borderSection text-[white] bg-black pt-[56px] px-[32px] sm:py-[72px]">
         <div className="lg:max-w-[calc(83.33333333333334%-6rem)] lg:mx-[auto]">
           <h2 className="text-center text-[2rem] md:text-[3rem] font-[700] mb-[24px]">
-            Foire aux questions
+            {language === "fr" ? "Foire aux questions": "Frequently Asked Questions"}            
           </h2>
           <ul>
-            {  AccordionDatas.map((data, index) => 
+            {  faq?.map((data, index) => 
             <Accordion key={index} question={data.question} text1={data.text1} text2={data.text2}/>
             )}
           </ul>
           <div className="bg-black pt-[2.85rem]">
-            <h3 className="text-[white] text-center px-[64px]">Prêt à regarder Netflix ? Saisissez votre adresse e-mail pour vous abonner ou réactiver votre abonnement.</h3>
+            <h3 className="text-[white] text-center px-[64px]">{
+              language === "fr" ?
+            "Prêt à regarder Netflix ? Saisissez votre adresse e-mail pour vous abonner ou réactiver votre abonnement.": "Ready to watch? Enter your email to create or restart your membership."}</h3>
             <EmailForm />
           </div>
         </div>
